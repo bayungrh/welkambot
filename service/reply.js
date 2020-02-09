583059cc23bf1d319ab22824d4e9e93d7857df9f
bayun <bnugraha00@hotmail.com>
2020-02-09 18:38:26 +0700

    simplify reply service

--- service/reply.js
+++ service/reply.js
@@ -5,20 +5,13 @@ const baseUri = `https://api.telegram.org/bot${token}`
 
 const replyText = (chat_id, text) => {
     let endpoint = `${baseUri}/sendMessage`
-    return new Promise((resolve, reject) => {
-        unirest
+    return unirest
         .post(endpoint)
         .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
         .send({
             chat_id: chat_id,
             text: text
         })
-        .then((response) => {
-            return resolve(response)
-        }).catch(err => {
-            return reject(err)
-        })
-    })
 }
 
 module.exports = {
